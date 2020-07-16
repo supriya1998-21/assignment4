a1596de14478c601851265bd03cfc04c6014ce59
supriya21 <>
2020-07-16 14:44:42 +0530

    change

---
+++ script.js
@@ -0,0 +1,12 @@
+
+	function name(name){
+	if( "name =='j'" || "name =='J'"){
+		console.log("hii " + name);
+	}
+	else{
+		console.log("hello " + name);
+		};
+	};
+	console.log(name("supriya"));
+
+
